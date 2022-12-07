42ad1bcff05eb91e34d343c0d71be14249e02431
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2022-12-07 14:47:57 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 import './style.css';
 
 // Write Javascript code!
+
+var ms;
+
 navigator.mediaDevices.getUserMedia({ video: true }).then((ms) => {
   console.log(ms.getTracks()[0]);
-    setTimeout(()=>{
-      ms.getTracks()[0].stop;
-    },3000)
+  ms = ms;
 });
+
+setTimeout(() => {
+  ms.getTracks()[0].stop;
+}, 3000);
